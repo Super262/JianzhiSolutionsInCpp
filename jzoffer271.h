696038e7e668bdb72f98b8689f0a4a68dcc28d48
Super262 <virusformail@gmail.com>
2021-01-29 14:55:17 +0800

    Finished 27-1.

--- jzoffer271.h
+++ jzoffer271.h
@@ -7,3 +7,16 @@ struct TreeNode
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
+TreeNode *mirrorTree(TreeNode *root)
+{
+    if (!root)
+    {
+        return root;
+    }
+    TreeNode *l = root->left;
+    TreeNode *r = root->right;
+    root->left = mirrorTree(r);
+    root->right = mirrorTree(l);
+    return root;
+}
