f0801adc15cb90b89076d973a75123fb405b8aad
Super262 <virusformail@gmail.com>
2021-01-29 15:34:16 +0800

    Finished 19-1.

---
+++ jzoffer291.h
@@ -0,0 +1,92 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    const int height = matrix.size();
+    if (!height)
+    {
+        return vector<int>();
+    }
+    const int width = matrix[0].size();
+    if (!width)
+    {
+        return vector<int>();
+    }
+    vector<int> result;
+
+    int xBottom = 0;
+    int xTop = width - 1;
+    int yBottom = 0;
+    int yTop = height - 1;
+
+    int x = 0;
+    int y = 0;
+    int direction = 1;
+    while (x >= xBottom && x <= xTop && y >= yBottom && y <= yTop)
+    {
+        result.emplace_back(matrix[y][x]);
+        switch (direction)
+        {
+        case 1:
+        {
+            if (x == xTop)
+            {
+                direction = 2;
+                ++yBottom;
+                ++y;
+            }
+            else
+            {
+                ++x;
+            }
+            break;
+        }
+        case 2:
+        {
+            if (y == yTop)
+            {
+                direction = 3;
+                --xTop;
+                --x;
+            }
+            else
+            {
+                ++y;
+            }
+            break;
+        }
+        case 3:
+        {
+            if (x == xBottom)
+            {
+                direction = 4;
+                --yTop;
+                --y;
+            }
+            else
+            {
+                --x;
+            }
+            break;
+        }
+        case 4:
+        {
+            if (y == yBottom)
+            {
+                direction = 1;
+                ++xBottom;
+                ++x;
+            }
+            else
+            {
+                --y;
+            }
+            break;
+        }
+        }
+    }
+
+    return result;
+}
