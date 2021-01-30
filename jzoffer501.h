6231d862ad8a80079f3ca45ab325f2a64b268345
Super262 <virusformail@gmail.com>
2021-01-30 19:50:29 +0800

    Finished 50-1.

---
+++ jzoffer501.h
@@ -0,0 +1,38 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+char firstUniqChar(string s)
+{
+    const int sLen = s.size();
+    char result = ' ';
+    if (sLen == 0)
+    {
+        return result;
+    }
+    map<char, int> chToIndex;
+    for (int i = 0; i < sLen; ++i)
+    {
+        if (chToIndex.find(s[i]) == chToIndex.end())
+        {
+            chToIndex[s[i]] = i;
+        }
+        else
+        {
+            chToIndex[s[i]] = sLen;
+        }
+    }
+    int minI = sLen;
+    for (map<char, int>::iterator iter = chToIndex.begin(); iter != chToIndex.end(); ++iter)
+    {
+        if (iter->second < minI)
+        {
+            minI = iter->second;
+            result = iter->first;
+        }
+    }
+    return result;
+}
