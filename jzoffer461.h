7aa8067413e42aa55a7511eb04cc6f8de200d05c
Super262 <virusformail@gmail.com>
2021-01-30 16:34:41 +0800

    finished 46-1.

---
+++ jzoffer461.h
@@ -0,0 +1,36 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int translateNum(int num)
+{
+    if (num < 10)
+    {
+        return 1;
+    }
+    if (num < 100)
+    {
+        if (num > 25)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+
+    int res = 0;
+
+    res += translateNum(num / 10);
+
+    int twoDigit = num % 100;
+    if (twoDigit > 9 && twoDigit < 26)
+    {
+        res += translateNum(num / 100);
+    }
+
+    return res;
+}
